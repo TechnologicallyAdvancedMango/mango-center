1531e04975f257b7329b93b24e71a3ef968c4cb0
techmango <kevinesko@hotmail.com>
2025-08-29 22:23:28 -0400

    Update clicker.js

--- globalClicker/clicker.js
+++ globalClicker/clicker.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { supabase } from './supabaseClient.js';
 
 let clickRowId = null;
 
