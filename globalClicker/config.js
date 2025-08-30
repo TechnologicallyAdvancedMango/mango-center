15b409e371ad66de1085ae912d6713cd5fd25418
techmango <kevinesko@hotmail.com>
2025-08-29 21:20:39 -0400

    Update config.js

--- globalClicker/config.js
+++ globalClicker/config.js
@@ -1,4 +1,3 @@
-// config.js
-const SUPABASE_URL = 'https://lifeczpvyqpffvmnjzrl.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZmVjenB2eXFwZmZ2bW5qenJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MjU3ODUsImV4cCI6MjA3MjAwMTc4NX0.k-Ppi_8sRvP5mQWBrTLF_n2ZLR6oADSvOg0uEZLNIDg';
-const CLICKER_ID = 'global';
+export const SUPABASE_URL = 'https://lifeczpvyqpffvmnjzrl.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZmVjenB2eXFwZmZ2bW5qenJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MjU3ODUsImV4cCI6MjA3MjAwMTc4NX0.k-Ppi_8sRvP5mQWBrTLF_n2ZLR6oADSvOg0uEZLNIDg';
+export const CLICKER_ID = 'global';
