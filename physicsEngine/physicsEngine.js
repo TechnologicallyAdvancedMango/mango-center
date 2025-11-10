90f6a8c6db292cf85b56b9b72a1dcffd4c1be7b1
techmango <kevinesko@hotmail.com>
2025-11-10 18:33:42 -0500

    Update physicsEngine.js

--- physicsEngine/physicsEngine.js
+++ physicsEngine/physicsEngine.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
 let fps = 60;
 let frameMultiplier = 1;
 
