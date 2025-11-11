58cb7a6e0021ec61c08f54ddfcd3c931819ce4d7
techmango <kevinesko@hotmail.com>
2025-11-11 08:35:43 -0500

    Update physicsEngine.js

--- physicsEngine/physicsEngine.js
+++ physicsEngine/physicsEngine.js
@@ -401,8 +401,8 @@ let circle1 = new Circle(400, 200, 25);
 let circle2 = new Circle(700, 200, 25);
 let circle3 = new Circle(700, 400, 25);
 
-let spring1 = new Spring(circle1, circle2, 350, 0.1, 0.5);
-let spring2 = new Spring(circle2, circle3, 350, 0.1, 0.5);
+// let spring1 = new Spring(circle1, circle2, 350, 0.1, 0.5);
+// let spring2 = new Spring(circle2, circle3, 350, 0.1, 0.5);
 // let spring3 = new Spring(circle3, circle1, 350, 0.1, 0.5);
 
 let groundHeight = canvas.height * 0.2;
