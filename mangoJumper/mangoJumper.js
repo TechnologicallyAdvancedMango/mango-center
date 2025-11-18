5748030b6fe1b4c1dd88e4e806c559cf97378f1a
techmango <kevinesko@hotmail.com>
2025-11-18 09:25:33 -0500

    Update mangoJumper.js

--- mangoJumper/mangoJumper.js
+++ mangoJumper/mangoJumper.js
@@ -9,7 +9,7 @@ Add more gamemodes/portals
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let gravity = 0.3;
+let gravity = 1;
 let speed = 5;
 let gameSpeed = 1;
 
