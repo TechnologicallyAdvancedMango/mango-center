bdfcf3f9addea414e9352f93830ca77db2489867
techmango <kevinesko@hotmail.com>
2025-11-18 15:25:02 -0500

    Update mangoJumper.js

--- mangoJumper/mangoJumper.js
+++ mangoJumper/mangoJumper.js
@@ -223,7 +223,7 @@ class Player {
 
             if (gravity > 0) {
                 // --- Normal gravity: land on top ---
-                if (this.vy > 0 && hb.y + hb.height <= block.y + 10) {
+                if (this.vy > 0 && hb.y + hb.height <= block.y + 1) {
                     this.y = block.y - this.height;
                     this.vy = 0;
                     this.onGround = true;
