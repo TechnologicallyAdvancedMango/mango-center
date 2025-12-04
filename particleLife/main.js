f2b2505929abbb81832826470f188838d03ec672
techmango <kevinesko@hotmail.com>
2025-12-03 21:38:48 -0500

    decrease forceFactor

--- particleLife/main.js
+++ particleLife/main.js
@@ -7,7 +7,7 @@ canvas.height = Math.floor(canvas.clientHeight);
 
 
 const maxRadius = 0.1;
-const forceFactor = 25;
+const forceFactor = 20;
 
 const cellSize = maxRadius;
 const gridWidth  = Math.floor(1 / cellSize);
