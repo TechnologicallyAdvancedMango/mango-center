cc8d3ecb713e784a3d0d5401918586fa450c3879
techmango <kevinesko@hotmail.com>
2025-12-04 09:14:22 -0500

    Update main.js

--- particleLife/main.js
+++ particleLife/main.js
@@ -6,8 +6,8 @@ canvas.width  = Math.floor(canvas.clientWidth);
 canvas.height = Math.floor(canvas.clientHeight);
 
 
-const maxRadius = 0.05;
-const forceFactor = 30;
+const maxRadius = 0.075;
+const forceFactor = 20;
 
 const cellSize = maxRadius;
 const gridWidth  = Math.floor(1 / cellSize);
