7a0cb415b2f48037b533085829ff0cb591489d66
TechnologicallyAdvancedMango <erikesko2@gmail.com>
2026-02-21 22:59:07 -0500

    wow cpu

--- virtualMachine/index.js
+++ virtualMachine/index.js
@@ -1,13 +1,27 @@
 const memory = new Uint8Array(256);
 const registers = new Uint8Array(8);
 let pc = 0; // Program counter
+let interval;
+
+memory[0] = 0x01;
+memory[1] = 0x02;
+memory[2] = 0x01;
 
 function clock() {
     const opCode = memory[pc];
     pc++;
 
     switch (opCode) {
-        case 0x01: 
+        case 0x00:
+            break;
+
+        case 0xFF:
+            console.log("HALT");
+            clearInterval(interval);
+            break;
+
+
+        case 0x01:
             console.log("Hello World");
             break;
 
@@ -21,4 +35,4 @@ function clock() {
     }
 }
 
-setInterval(clock, 1000);
+interval = setInterval(clock, 1000);
